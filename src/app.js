01e5cd2265b10a65ed570ad3ff0025e27385df61
Priscila Hidaka Favaro <48254537+priscilahf@users.noreply.github.com>
2021-08-02 14:12:25 +0100

    Temperature and city elements

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+}
+
 let apiKey = "4dd3862c2f0dfdc275854923e14a1ea3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=${apiKey}&units=metric`;
-console.log(apiKey);
+
 console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
