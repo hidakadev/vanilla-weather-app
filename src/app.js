47a519024801aec3bafdbcbe68b82813aad22282
Priscila Hidaka Favaro <48254537+priscilahf@users.noreply.github.com>
2021-08-02 14:35:30 +0100

    Additional weather info

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let tempmaxElement = document.querySelector("#temp-max");
+    let tempminElement = document.querySelector("#temp-min");
+    let feelslikeElement = document.querySelector("#feels-like");
+    let humidityElement = document.querySelector("#humidity");
+    let windspeedElement = document.querySelector("#wind-speed");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
+    tempmaxElement.innerHTML = Math.round(response.data.main.temp_max);
+    tempminElement.innerHTML = Math.round(response.data.main.temp_min);
+    feelslikeElement.innerHTML = Math.round(response.data.main.feels_like);
+    humidityElement.innerHTML = Math.round(response.data.main.humidity);
+    windspeedElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "4dd3862c2f0dfdc275854923e14a1ea3";
