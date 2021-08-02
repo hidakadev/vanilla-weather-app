679ca40aa6c211d0bc2bf231d0acc0a03b19dd10
Priscila Hidaka Favaro <48254537+priscilahf@users.noreply.github.com>
2021-08-02 14:01:19 +0100

    API key and URL

---
+++ src/app.js
@@ -0,0 +1,4 @@
+let apiKey = "4dd3862c2f0dfdc275854923e14a1ea3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=${apiKey}&units=metric`;
+console.log(apiKey);
+console.log(apiUrl);
